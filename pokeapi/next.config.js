3af348b6ef1ac4913f48c21e3d7d60595471fd74
Thiago Luiz Biffi <thiago.biffi@gmail.com>
2021-10-28 23:02:59 -0300

    Adicionando styled components & inicio da internacionalidação

--- pokeapi/next.config.js
+++ pokeapi/next.config.js
@@ -1,3 +1,9 @@
 module.exports = {
+  i18n: {
+    locales: ['en-US', 'fr', 'nl-NL', 'pt-BR'],
+    // This is the default locale you want to be used when visiting
+    // a non-locale prefixed path e.g. `/hello`
+    defaultLocale: 'en-US',
+  },
   reactStrictMode: true,
 }
