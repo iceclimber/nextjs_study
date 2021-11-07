a9bfb212e7987c0f30a3788497625bc617267a4c
Thiago Luiz Biffi <thiago.biffi@gmail.com>
2021-11-07 00:11:12 -0300

    Adicionando BuildId fixa

--- pokeapi/next.config.js
+++ pokeapi/next.config.js
@@ -2,5 +2,9 @@ module.exports = {
   reactStrictMode: true,
   images: {
     domains: ['raw.githubusercontent.com']
-  }
+  },
+  generateBuildId: async () => {
+    // You can, for example, get the latest git commit hash here
+    return 'PokeApi_Next.JS'
+  },
 }
