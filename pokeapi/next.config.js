7d027c609c729d4a201813def2e0b2a0dec85f0a
Thiago Luiz Biffi <thiago.biffi@gmail.com>
2021-11-06 22:07:55 -0300

    removendo locale temporariamente

--- pokeapi/next.config.js
+++ pokeapi/next.config.js
@@ -1,9 +1,3 @@
 module.exports = {
-  i18n: {
-    locales: ['en-US', 'fr', 'nl-NL', 'pt-BR'],
-    // This is the default locale you want to be used when visiting
-    // a non-locale prefixed path e.g. `/hello`
-    defaultLocale: 'en-US',
-  },
   reactStrictMode: true,
 }
