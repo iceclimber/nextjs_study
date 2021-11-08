e757c22df4f719b4cc67d201c25fd71d226fd898
Thiago Luiz Biffi <thiago.biffi@gmail.com>
2021-11-07 21:53:53 -0300

    removendo informações ainda não exibidas

--- pokeapi/pages/api/pokemon/[pokemon_name].js
+++ pokeapi/pages/api/pokemon/[pokemon_name].js
@@ -2,5 +2,5 @@ export default async function handler(req, res) {
     let { pokemon_name } = req.query
     let pokemon_info = await fetch(`${process.env.POKEMON_API}/pokemon/${pokemon_name}`, { headers: { method: 'GET', 'Content-Type': 'application/json' } })
     pokemon_info = await pokemon_info.json()
-    res.status(200).send(pokemon_info)
+    res.status(200).send({ name: pokemon_info.name, sprites: pokemon_info.sprites })
 }
